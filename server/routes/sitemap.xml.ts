64861d800553e5c4d021fe1539a22bd50077bf8c
Sam Carrington <84904719+samcarrington@users.noreply.github.com>
2024-09-24 21:21:04 +0100

    fix: null defence

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -30,9 +30,9 @@ function getStaticEndpoints(): string[] {
     .filter((file) => !file.includes("slug")) // exclude dynamic content
     .map((file) => file.split("pages")[1])
     .map((file) => {
-      return file.endsWith("index.vue")
+      return file?.endsWith("index.vue")
         ? file.split("/index.vue")[0]
-        : file.split(".vue")[0];
+        : file?.split(".vue")[0];
     });
 }
 
